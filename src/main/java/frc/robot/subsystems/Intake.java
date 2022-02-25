86d3d44f5095874e3e2b5361acc9f185bc81f9ae
Angela-Dang <angelathuyanhdang@gmail.com>
2022-02-24 16:49:33 -0800

    intake finished

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,13 +5,31 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
 
-  @Override
-  public void periodic() {
+  /** Creates a new Intake. */
+  TalonSRX intakeMotor = new TalonSRX (14);
+
+  public Intake() {
+
+    intakeMotor.setNeutralMode(NeutralMode.Coast);
+
+  }
+
+  public void setSpeed (double speed) {
+
+    intakeMotor.set(TalonSRXControlMode.PercentOutput, speed);
+
+  }
+
+  @Override public void periodic () {
+    
     // This method will be called once per scheduler run
+
   }
 }
+
