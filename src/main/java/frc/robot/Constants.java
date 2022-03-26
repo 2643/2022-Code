379fae50b0bc776edc535b2466853e82deda98fe
RayirthDinesh <91096595+RayirthDinesh@users.noreply.github.com>
2022-03-26 15:20:48 -0700

    Shashhank's turret code, but it is only tested for the 2020

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,43 +1,52 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+// package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
+// import edu.wpi.first.networktables.NetworkTable;
+// import edu.wpi.first.networktables.NetworkTableEntry;
+// import edu.wpi.first.networktables.NetworkTableInstance;
+// import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+// import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {
-    public static final int DRIVETRAIN_FRONT_LEFT_MOTOR = 3;
-    public static final int DRIVETRAIN_BACK_LEFT_MOTOR = 4;
+// /**
+//  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+//  * constants. This class should not be used for any other purpose. All constants should be declared
+//  * globally (i.e. public static). Do not put anything functional in this class.
+//  *
+//  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+//  * constants are needed, to reduce verbosity.
+//  */
+// public final class Constants {
+//     public static final int DRIVETRAIN_FRONT_LEFT_MOTOR = 3;
+//     public static final int DRIVETRAIN_BACK_LEFT_MOTOR = 4;
 
-    public static final int DRIVETRAIN_FRONT_RIGHT_MOTOR = 1;
-    public static final int DRIVETRAIN_BACK_RIGHT_MOTOR = 2;
+//     public static final int DRIVETRAIN_FRONT_RIGHT_MOTOR = 1;
+//     public static final int DRIVETRAIN_BACK_RIGHT_MOTOR = 2;
 
-    public static final int JOYSTICK_LEFT_AXIS = 1;
-    public static final int JOYSTICK_RIGHT_AXIS = 5;
-    public static final double JOYSITCK_DEADBAND = 0.025;
-    public static boolean slowMode = false;
-    public static final double SLOW_MODE_MULTIPLIER = 0.5;
+//     public static final int JOYSTICK_LEFT_AXIS = 1;
+//     public static final int JOYSTICK_RIGHT_AXIS = 5;
+//     public static final double JOYSITCK_DEADBAND = 0.025;
+//     public static boolean slowMode = false;
+//     public static final double SLOW_MODE_MULTIPLIER = 0.5;
 
-    public static final int rightClimberPort = 0; //add port num later
-    public static final int leftClimberPort = 0; //add port num later
-    public static final double maxpos = 0; //test for max speed later
+//     public static final int rightClimberPort = 0; //add port num later
+//     public static final int leftClimberPort = 0; //add port num later
+//     public static final double maxpos = 0; //test for max speed later
     
-    public static final int leftShooterPort = 5;
-    public static final int rightShooterPort = 6;
-    public static final int conveyorBeltMotorPort = 15;
+//     public static final int leftShooterPort = 5;
+//     public static final int rightShooterPort = 6;
+//     public static final int conveyorBeltMotorPort = 15;
 
-    public static final int TurretMotorPort = 7;
-    public static final int turretLimitSwitchPort = 0;
-    public static final double defaultVisionTurretError = 0;
-    public static NetworkTable visionTable = NetworkTableInstance.getDefault().getTable("vision-movement");
-}
+//     public static final int TurretMotorPort = 7;
+//     public static final int turretLimitSwitchPort = 0;
+//     public static final double defaultVisionTurretError = 0;
+//     public static NetworkTable visionTable = NetworkTableInstance.getDefault().getTable("vision-movement");
+//     public static int visionTurretError = 10;
+//     public static ShuffleboardTab TalonFXTab = Shuffleboard.getTab("TalonFX");
+//     public static NetworkTableEntry wantedPositionTurret = TalonFXTab.add("Wanted Position", 0).getEntry();
+//     public static NetworkTableEntry pidError =  TalonFXTab.add("PID Error", 0).getEntry();
+//     public static NetworkTableEntry degrees = TalonFXTab.add("Degrees", 0).getEntry();
+//     //public static double turretPosition = 0;
+// }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,72 +1,57 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+// package frc.robot;
 
+// import edu.wpi.first.wpilibj.GenericHID;
+// import edu.wpi.first.wpilibj.Joystick;
+// import edu.wpi.first.wpilibj.XboxController;
+// import frc.robot.commands.Turret.turretShoot;
+// import frc.robot.subsystems.TurretSubsystem;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
+// /**
+//  * This class is where the bulk of the robot should be declared. Since Command-based is a
+//  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+//  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+//  * subsystems, commands, and button mappings) should be declared here.
+//  */
+// public class RobotContainer {
+//   // The robot's subsystems and commands are defined here...
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.Turret.turretShoot;
-import frc.robot.subsystems.Turret;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+//   public static TurretSubsystem m_turret = new TurretSubsystem();
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
-public class RobotContainer {
+//   public static Joystick driveStick = new Joystick(0);
+//   public static JoystickButton turretTest = new JoystickButton(driveStick, 1);
 
-
-  // The robot's subsystems and commands are defined here...
-
-  //public static final Drivetrain m_drivetrain = new Drivetrain();
-
-  public static Turret m_turret = new Turret();
-
-  public static Joystick driveStick = new Joystick(0);
-
-  public static Joystick opboard = new Joystick(1);
-  public static JoystickButton shootButton = new JoystickButton(opboard, 3);
+//   //public static Joystick opBoard = new Joystick(1);
+//   //public static JoystickButton shootButton = new JoystickButton(opBoard, 1);
 
   
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() 
-  {
-    shootButton.whileHeld(new turretShoot());
-  }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return null;
-  }
+//   /** The container for the robot. Contains subsystems, OI devices, and commands. */
+//   public RobotContainer() {
+//     // Configure the button bindings
+//     configureButtonBindings();
+//   }
 
-  public static boolean button1() {
-    return false;
-  }
+//   /**
+//    * Use this method to define your button->command mappings. Buttons can be created by
+//    * instantiating a {@link GenericHID} or one of its subclasses ({@link
+//    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+//    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+//    */
+//   private void configureButtonBindings() {
+//     //turretTest.whenPressed(new turretShoot(), false);
+//     turretTest.whenPressed(new turretShoot());
+//   }
 
-public static boolean button2() {
-    return false;
-}
-}
+//   /**
+//    * Use this to pass the autonomous command to the main {@link Robot} class.
+//    *
+//    * @return the command to run in autonomous
+//    */
+// }
